188a533c8a86691b1ee9e0bcdc7d5dd61cb35f48
Wade <158378370+wadevenz@users.noreply.github.com>
2024-08-13 20:59:00 +1000

    feat: log bananas

---
+++ exampleJavaScript.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+
+
+
+
+let someMessage = "Bananas!"; 
+
+
+console.log(someMessage);
+
+
+someMessage = 1000;
+
+
+
+console.log(someMessage);
+
+
+console.log("Goodbye World!");
