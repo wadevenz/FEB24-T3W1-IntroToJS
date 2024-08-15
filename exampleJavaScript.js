264b89335c509022121782fcb319ee24ae1c27a4
Wade <158378370+wadevenz@users.noreply.github.com>
2024-08-15 20:06:15 +1000

    feat of conditional stuff

--- exampleJavaScript.js
+++ exampleJavaScript.js
@@ -1,19 +1,82 @@
-console.log("Hello World!");
-
-
-
-
 let someMessage = "Bananas!"; 
 
+if (someMessage) {
+    console.log("Conditional log");
+}
 
-console.log(someMessage);
+if (someMessage == "oranges") {
+    console.log("we have oranges!");
+}
+
+let someNumberMessage = "10";
+
+if (someNumberMessage === 10) {
+    console.log("We have 10 of the things!");
+}
+
+if (someNumberMessage >= 5) {
+    console.log("Number is greater than 5");
+}
+
+let someCoolMessage = (someMessage == "oranges") ? someMessage.substring(0, 2).toLocaleUpperCase() : "Default message here";
+
+console.log(someCoolMessage);
+
+if (someMessage == "oranges") {
+    let someCoolMessage = someMessage.substring(0, 2).toLocaleUpperCase() + "inside the if statement";
+    console.log(someCoolMessage);
+} else {
+    let someCoolMessage = "Default message here inside the else statement";
+    console.log(someCoolMessage);
+}
+
+console.log("someCoolMessage is now: " + someCoolMessage);
 
 
-someMessage = 1000;
+let fruitChoices = ["oranges", "bananas", "mangoes"];
+let fruitChoicesAsObject = {
+    oranges: "oranges",
+    bananas: "bananas",
+    mangoes: "mangoes"
+}
+
+let fruit = fruitChoicesAsObject.bananas;
+
+switch (fruit) {
+    case fruitChoicesAsObject.bananas:
+        
+        break;
+    case fruitChoicesAsObject.oranges:
+        
+        break;
+    default:
+        break;
+}
 
 
 
-console.log(someMessage);
+someMessage = "bananas";
+switch (someMessage) {
+    case "oranges":
+    console.log("We have oranges")
+    break;
+    case "bananas":
+    console.log("We have bananas")
+    break;
+    case "mangoes":
+    console.log("We have mangoes")
+    break;
+    default:
+    console.log("We have some exotic fruit!")
+    break;
+}
 
+someCoolMessage = false;
 
-console.log("Goodbye World!");
+let someShortCircuitLogicValue = (someMessage && someCoolMessage) && "super cool short circuit logic string";
+
+console.log(someShortCircuitLogicValue);
+
+let someOrCircuitLogicValue = someMessage || someCoolMessage || "super cool OR operator logic string";
+
+console.log(someOrCircuitLogicValue);
